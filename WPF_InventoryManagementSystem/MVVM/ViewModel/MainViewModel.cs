ffee98059b14be11354e9809ec6090525a5aaf92
cdoll40 <cdoll831@gmail.com>
2023-05-31 15:31:38 -0500

    Add project files.

---
+++ WPF_InventoryManagementSystem/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPF_InventoryManagementSystem.Core;
+
+namespace WPF_InventoryManagementSystem.MVVM.ViewModel {
+    internal class MainViewModel : ObservableObject {
+
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand DiscoveryViewCommand { get; set; }
+
+        public HomeViewModel HomeVM { get; set; }
+        public InventoryViewModel DiscoveryVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView {
+            get { return _currentView; }
+            set {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel() {
+
+            HomeVM = new HomeViewModel();
+            DiscoveryVM = new InventoryViewModel();
+
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o => {
+                CurrentView = HomeVM;
+            });
+
+            DiscoveryViewCommand = new RelayCommand(o => {
+                CurrentView = DiscoveryVM;
+            });
+        }
+    }
+}
