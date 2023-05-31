9cf0fc4f8853a1a0a97ffc684ce79c84d29fc378
cdoll40 <cdoll831@gmail.com>
2023-05-31 17:28:07 -0500

    Inventory page implemented with a tab for each table

---
+++ WPF_InventoryManagementSystem/MVVM/ViewModel/MainInvViewmodel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPF_InventoryManagementSystem.Core;
+
+namespace WPF_InventoryManagementSystem.MVVM.ViewModel {
+    internal class MainInvViewModel : ObservableObject {
+
+        public RelayCommand CategoriesViewCommand { get; set; }
+        public RelayCommand ProductsViewCommand { get; set; }
+
+        public CategoriesViewModel CategoriesVM { get; set; }
+        public ProductsViewModel ProductsVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView {
+            get { return _currentView; }
+            set {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainInvViewModel() {
+
+            CategoriesVM = new CategoriesViewModel();
+            ProductsVM = new ProductsViewModel();
+
+            CurrentView = CategoriesVM;
+
+            CategoriesViewCommand = new RelayCommand(o => {
+                CurrentView = CategoriesVM;
+            });
+
+            ProductsViewCommand = new RelayCommand(o => {
+                CurrentView = ProductsVM;
+            });
+        }
+    }
+}
